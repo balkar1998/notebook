aa7161e0943a0fa69a58073614cfd29a27032d64
balkar1998 <balkar48812@gmail.com>
2023-08-16 22:13:40 +0530

    connect to database

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://localhost:27017/'; // mongodb://localhost:27017/ is the default path where MongoDB listens for connections.
+
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('Connected to MongoDB haha ');
+}
+
+module.exports = connectToMongo;
