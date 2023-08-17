ce29c56a48db52688d0ca733f3e001deb4a31c72
balkar1998 <balkar48812@gmail.com>
2023-08-17 15:22:40 +0530

    add validation on saving route

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/'; // mongodb://localhost:27017/ is the default path where MongoDB listens for connections.
+const mongoURI = 'mongodb://localhost:27017/notebook'; // mongodb://localhost:27017/ is the default path where MongoDB listens for connections.
 
 const connectToMongo = async () => {
     await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-    const user = User(req.body);
-    user.save()
+router.post("/", [
+    body("name", "Enter a valid name").isLength({ min: 5 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({ min: 7 }),
+] ,(req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // create new user
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then((user) => res.json(user))
+    .catch((error) => {
+        console.log(error);
+        res.json({ error: "Please enter unique value for email", success: false });
+    });
 });
 
 module.exports = router;
