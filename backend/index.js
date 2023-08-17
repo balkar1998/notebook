74bfadbeb625fbca3d20d958dd3971558bd5e21b
balkar1998 <balkar48812@gmail.com>
2023-08-17 15:45:05 +0530

    remove extra code

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 connectToMongo();
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // middleware
 app.use(express.json());
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,23 +7,29 @@ router.post("/", [
     body("name", "Enter a valid name").isLength({ min: 5 }),
     body("email", "Enter a valid email").isEmail(),
     body("password", "Password must be atleast 5 characters").isLength({ min: 7 }),
-] ,(req, res) => {
+] , async (req, res) => {
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        if (user) {
+            return res.status(400).json({ error: "Sorry a user with this email already exists" });
+        }
 
-    // create new user
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    }).then((user) => res.json(user))
-    .catch((error) => {
-        console.log(error);
-        res.json({ error: "Please enter unique value for email", success: false });
-    });
+        // create new user    
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
 });
 
 module.exports = router;
