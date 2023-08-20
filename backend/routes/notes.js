1134d6766965bca9858a58d9be1b161deeaad26c
balkar1998 <balkar48812@gmail.com>
2023-08-20 11:47:49 +0530

    update note end point

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -32,4 +32,39 @@ router.post('/addnote', fetchuser, [
     }
 });
 
+// Route 3: Update an existing note using: PUT "/api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const { title, description, tag } = req.body;
+        // Create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+        // Find the note to be updated and update it
+        // const note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+
+        let note = await Note.findById(req.params.id);
+        if (!note) { return res.status(404).send("Not Found") };
+
+        if (note.user.toString() !== req.user) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json({ note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occured");
+    }
+});
+
 module.exports = router; 
