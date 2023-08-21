c7c07d5dabb407dd60d73b56499c365c33d4a015
balkar1998 <balkar48812@gmail.com>
2023-08-22 01:47:52 +0530

    start creating components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+      </Router>
+    </>
   );
 }
 
